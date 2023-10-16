c7db896f32be27b321a23de945cb747a9ae94531
Anshul <anshul5khairnar@gmail.com>
2023-10-16 01:13:38 -0400

    Reminder Functionality added

---
+++ functionality/DisplayFreeTime.py
@@ -0,0 +1,157 @@
+from datetime import datetime
+from Event import Event
+import os
+import csv
+from datetime import timedelta
+from datetime import time
+from functionality.export_file import load_key, encrypt_file, decrypt_file
+
+
+async def get_free_time(ctx, bot):
+    """
+    Function:
+        get_free_time
+    Description:
+        giving the user the free time today according to the registered events by calling the function compute_free_time
+    Input:
+        ctx - Discord context window
+        bot - Discord bot user
+    Output:
+        - A message sent to the user channel stating every free time slot that is avaliable today
+    """
+    channel = await ctx.author.create_dm()
+
+    # check if the user has a completely empty schedule
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + str(ctx.author.id) + ".csv"):
+        await channel.send('You do not have any event at all')
+        return
+
+    rows = []
+
+    # decrypting the file
+    key = load_key(str(ctx.author.id))
+    decrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + str(ctx.author.id) + ".csv")
+
+    # open the calendar file and read the events
+    with open(
+            os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + str(ctx.author.id) + ".csv", "r"
+    ) as calendar_lines:
+        calendar_lines = csv.reader(calendar_lines, delimiter=",")
+
+        for row in calendar_lines:
+            if len(row) > 0:
+                rows.append(row)
+
+    calendarDates = []
+    rows.pop(0)
+    for i in rows:
+        if len(i) > 0:
+            datetime.strptime(i[2], "%Y-%m-%d %H:%M:%S"),
+            datetime.strptime(i[3], "%Y-%m-%d %H:%M:%S"),
+            temp_event = Event(
+                "",
+                datetime.strptime(i[2], "%Y-%m-%d %H:%M:%S"),
+                datetime.strptime(i[3], "%Y-%m-%d %H:%M:%S"),
+                "",
+                "",
+                "",
+            )
+            calendarDates.append(temp_event)
+    output = compute_free_time(calendarDates)
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + str(ctx.author.id) + ".csv")
+    await channel.send(output)
+
+
+def compute_free_time(calendarDates):
+    """
+    Function:
+        compute_free_time
+    Description:
+        returning a string that contains the user the free time according to the registered events
+    Input:
+        calendarDates - list of the events that the user has in the calendar
+    Output:
+        - a string stating every free time slot that is avaliable today
+    """
+
+    # defining  variable for the output string that the function will returns
+    output = ''
+
+    # two constants for the time at the start and end of the day
+    start_time = time(hour=0, minute=0)
+    end_time = time(hour=23, minute=58)
+
+    # constant = 1 minute in time
+    one_min = timedelta(hours=0, minutes=1)
+
+    # setting up variables for the following dates: today , start and end of this current week
+
+    today = datetime.today()
+
+    start_week = today - timedelta(today.weekday())
+    end_week = start_week + timedelta(7)
+
+    # adding only the events that are happening today
+    today_events = []
+    for e in calendarDates:
+        if e.start_date.date() == today.date() or e.end_date.date() == today.date():
+            today_events.append(e)
+
+    # check if the user has no event for today
+
+    if len(today_events) == 0:
+        return 'You do not have any event for today'
+    # checks if the user only has one event for today and it starts at 00:00
+    elif len(today_events) == 1 and today_events[0].start_date.time() == start_time:
+        free_time_start = (today_events[0].end_date + one_min).time()
+        return 'Free time from ' + str(free_time_start) + ' until end of the day\n'
+    # Cases where the event doesn't start at 00:00
+    elif len(today_events) == 1:
+        return 'Free time from 00:00 until ' + str((today_events[0].start_date - one_min).time()) + ' and from ' + \
+               str((today_events[0].end_date + one_min).time()) + ' until end of the day\n'
+
+    # sorting today's events
+    today_events.sort()
+
+    # setting up the first possible free time
+    #
+    free_time_start = (today_events[0].end_date + one_min).time()
+    free_time_end = (today_events[1].start_date - one_min).time()
+
+    # removing the first event if it occurs exactly at 00:00 as there is no free time before it
+    # and also showing the first free time if there is a time between the end of the first event and the start of the second event
+    # if not , it will show the first free time which occurs exactly at 00:00
+
+    if today_events[0].start_date.time() == start_time and free_time_start < free_time_end:
+        output += ('Free time from ' +
+                   str(free_time_start) +
+                   ' until ' +
+                   str(free_time_end) + '\n')
+        today_events.pop(0)
+    else:
+        output += ('Free time from 00:00 until ' + str((today_events[0].start_date - one_min).time()) + '\n')
+
+    # showing free time by iterating through the list of events ignoring the last one as it needs a special case
+
+    for i in range(len(today_events) - 1):
+        free_time_start = (today_events[i].end_date + one_min).time()
+        free_time_end = (today_events[i + 1].start_date - one_min).time()
+        # ignoring if the free time is actually one minute or less
+        if free_time_start < free_time_end:
+            output += ("Free time from " +
+                       str(free_time_start) +
+                       ' until ' + str(free_time_end) + '\n')
+
+    # showing the last free time slot if the last event in the day doesn't end exactly at:23:59
+    if today_events[-1].end_date.time() < end_time:
+        output += ('Free time from ' +
+                   str((today_events[-1].end_date + one_min).time())
+                   + ' until 23:59 ')
+
+    return output
+
+
+
+
+
+
---
+++ functionality/shared_functions.py
@@ -0,0 +1,480 @@
+import os
+import csv
+from pathlib import Path
+from Event import Event
+from Reminder import Reminder
+from datetime import datetime
+from cryptography.fernet import Fernet
+
+
+def create_type_directory():
+    """
+    Function: create_type_directory
+    Description: Creates ScheduleBot type directory in users Documents folder if it doesn't exist
+    Input: None
+    Output: Creates Type folder if it doesn't exist
+    """
+
+    if not os.path.exists(os.path.expanduser("~/Documents/ScheduleBot/Type")):
+        Path(os.path.expanduser("~/Documents/ScheduleBot/Type")).mkdir(parents=True, exist_ok=True)
+
+
+def create_type_file(user_id):
+    """
+    Function: create_type_file
+    Description: Checks if the event type file exists, and creates it if it doesn't
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: Creates the event type file if it doesn't exist
+    """
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv"):
+        with open(
+            os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv",
+            "x",
+            newline="",
+        ) as new_file:
+            csvwriter = csv.writer(new_file, delimiter=",")
+            csvwriter.writerow(["Event Type", "Start time", "End time"])
+        key = check_key(user_id)
+        encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv")
+
+
+def create_type_tree(user_id):
+    """
+    Function: create_type_tree
+    Description: Checks if the event type directory and file exists, and creates them if they don't
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: Creates the event type folder and file if they don't exist
+    """
+    create_type_directory()
+    create_type_file(user_id)
+
+
+def read_type_file(user_id):
+    """
+    Function: read_type_file
+    Description: Reads the event type file
+    for those event types
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output:
+        rows - List of rows
+    """
+
+    key = load_key(user_id)
+    decrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv")
+
+
+    # Opens the event type file
+    with open(
+        os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv", "r"
+    ) as type_lines:
+        type_lines = csv.reader(type_lines, delimiter=",")
+        current_row = []
+        rows = []
+        for line in type_lines:
+            for text in line:
+                current_row.append(text)
+            rows.append(current_row)
+            current_row = []
+
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv")
+    return rows
+
+
+def turn_types_to_string(user_id):
+    """
+    Function: turn_types_to_string
+    Description: Reads the event types file and turns all of them into a formatted string
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output:
+        output - Formatted string of rows in event types file
+    """
+    output = ""
+    space = [12, 5, 5]
+    rows = read_type_file(user_id)
+    line_number = 0
+    for i in rows:
+        if line_number != 0:
+            output += f"{i[0]:<{space[0]}} Preferred range of {i[1]:<{space[1]}} - {i[2]:<{space[2]}}\n"
+        line_number += 1
+    return output
+
+
+def create_event_directory():
+    """
+    Function: create_event_directory
+    Description: Creates ScheduleBot event directory in users Documents folder if it doesn't exist
+    Input: None
+    Output: Creates Event folder if it doesn't exist
+    """
+    if not os.path.exists(os.path.expanduser("~/Documents/ScheduleBot/Event")):
+        Path(os.path.expanduser("~/Documents/ScheduleBot/Event")).mkdir(parents=True, exist_ok=True)
+
+
+def create_event_file(user_id):
+    """
+    Function: create_event_file
+    Description: Checks if the calendar file exists, and creates it if it doesn't
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: Creates the calendar file if it doesn't exist
+    """
+    # Checks if the calendar file exists, and creates it if it doesn't
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv"):
+        with open(
+            os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv",
+            "x",
+            newline="",
+        ) as new_file:
+            csvwriter = csv.writer(new_file, delimiter=",")
+            csvwriter.writerow(["ID", "Name", "Start Date", "End Date", "Priority", "Type", "Notes","Location"])
+
+        key = check_key(user_id)
+        encrypt_file(key , os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv")
+
+def create_event_tree(user_id):
+    """
+    Function: create_event_tree
+    Description: Checks if the calendar directory and file exists, and creates them if they don't
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: Creates the calendar folder and file if they don't exist
+    """
+    create_event_directory()
+    create_event_file(user_id)
+
+
+def read_event_file(user_id):
+    """
+    Function: read_event_file
+    Description: Reads the calendar file and creates a list of rows
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output:
+        rows - List of rows
+    """
+
+    key = load_key(user_id)
+    decrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv")
+
+    # Opens the current user's csv calendar file
+    with open(os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv", "r") as calendar_lines:
+        calendar_lines = csv.reader(calendar_lines, delimiter=",")
+        rows = []
+        # Stores the current row in an array of rows if the row is not a new-line character
+        # This check prevents an accidental empty lines from being kept in the updated file
+        for row in calendar_lines:
+            if len(row) > 0:
+                rows.append(row)
+
+
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv")
+    return rows
+
+def add_event_to_file(user_id, current):
+    """
+    Function: add_event_to_file
+    Description: Adds an event to the calendar file in chronological order
+    Input:
+        user_id - String representing the Discord ID of the user
+        current - Event to be added to calendar
+    Output: None
+    """
+    line_number = 0
+    rows = read_event_file(user_id)
+    # If the file already has events
+    if len(rows) > 1:
+        for i in rows:
+            # Skips check with empty lines
+            if len(i) > 0 and line_number != 0:
+
+                # Temporarily turn each line into an Event object to compare with the object we are trying to add
+                temp_event = Event(
+                    "",
+                    datetime.strptime(i[2], "%Y-%m-%d %H:%M:%S"),
+                    datetime.strptime(i[3], "%Y-%m-%d %H:%M:%S"),
+                    "",
+                    "",
+                    "",
+                    "",
+                )
+                # If the current Event occurs before the temp Event, insert the current at that position
+                if current < temp_event:
+                    rows.insert(line_number, [""] + current.to_list())
+                    break
+
+                # If we have reached the end of the array and not inserted, append the current Event to the array
+                if line_number == len(rows) - 1:
+                    rows.insert(len(rows), [""] + current.to_list())
+                    break
+            line_number += 1
+    else:
+        rows.insert(len(rows), [""] + current.to_list())
+    # Open current user's calendar file for writing
+    with open(
+        os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv",
+        "w",
+        newline="",
+    ) as calendar_file:
+        # Write to column headers and array of rows back to the calendar file
+        csvwriter = csv.writer(calendar_file)
+        csvwriter.writerows(rows)
+
+    key = load_key(user_id)
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv")
+
+def delete_event_from_file(user_id, to_remove):
+    rows = read_event_file(user_id)
+    typeRows = read_type_file(user_id)
+    print("Rowns: "+rows.__str__())
+
+    for row in rows:
+        if to_remove['name'] == row[1]:
+            rows.remove(row)
+
+    for type_row in typeRows:
+        print("Type Row "+type_row.__str__())
+        if to_remove['desc'] == str(type_row[0]):
+            typeRows.remove(type_row)
+
+    # Open current user's calendar file for writing
+    with open(
+            os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv",
+            "w",
+            newline="",
+    ) as calendar_file:
+        # Write to column headers and array of rows back to the calendar file
+        csvwriter = csv.writer(calendar_file)
+        csvwriter.writerows(rows)
+
+    key = load_key(user_id)
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Event/" + user_id + ".csv")
+
+    with open(
+            os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv",
+            "w",
+            newline="",
+    ) as new_file:
+        csvwriter = csv.writer(new_file, delimiter=",")
+        csvwriter.writerows(typeRows)
+
+    key = check_key(user_id)
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Type/" + user_id + "event_types.csv")
+
+
+def create_type_directory():
+    """
+    Function: create_type_directory
+    Description: Creates ScheduleBot type directory in users Documents folder if it doesn't exist
+    Input: None
+    Output: Creates Type folder if it doesn't exist
+    """
+
+    if not os.path.exists(os.path.expanduser("~/Documents/ScheduleBot/Type")):
+        Path(os.path.expanduser("~/Documents/ScheduleBot/Type")).mkdir(parents=True, exist_ok=True)
+
+def create_reminder_directory():
+    """
+    Function: create_reminder_directory
+    Description: Creates ScheduleBot reminder directory in users Documents folder if it doesn't exist
+    Input: None
+    Output: Creates Type folder if it doesn't exist
+    """
+
+    if not os.path.exists(os.path.expanduser("~/Documents/ScheduleBot/Reminder")):
+        Path(os.path.expanduser("~/Documents/ScheduleBot/Reminder")).mkdir(parents=True, exist_ok=True)
+
+
+def create_reminder_file(user_id):
+    """
+    Function: create_reminder_file
+    Description: Checks if the reminder file exists, and creates it if it doesn't
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: Creates the reminder file if it doesn't exist
+    """
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Reminder/" + user_id + "reminder.csv"):
+        with open(
+            os.path.expanduser("~/Documents") + "/ScheduleBot/Reminder/" + user_id + "reminder.csv",
+            "x",
+            newline="",
+        ) as new_file:
+            csvwriter = csv.writer(new_file, delimiter=",")
+            csvwriter.writerow(["Description", "Time"])
+        key = check_key(user_id)
+        encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Reminder/" + user_id + "reminder.csv")
+
+
+def create_reminder_tree(user_id):
+    """
+    Function: create_reminder_tree
+    Description: Checks if the reminder directory and file exists, and creates them if they don't
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: Creates the reminder folder and file if they don't exist
+    """
+    create_reminder_directory()
+    create_reminder_file(user_id)
+
+
+def read_reminder_file(user_id):
+    """
+    Function: read_reminder_file
+    Description: Reads the reminder file
+    for those reminder
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output:
+        rows - List of rows
+    """
+
+    key = load_key(user_id)
+    decrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Reminder/" + user_id + "reminder.csv")
+
+
+    # Opens the event type file
+    with open(
+        os.path.expanduser("~/Documents") + "/ScheduleBot/Reminder/" + user_id + "reminder.csv", "r"
+    ) as type_lines:
+        type_lines = csv.reader(type_lines, delimiter=",")
+        current_row = []
+        rows = []
+        for line in type_lines:
+            for text in line:
+                current_row.append(text)
+            rows.append(current_row)
+            current_row = []
+
+    encrypt_file(key, os.path.expanduser("~/Documents") + "/ScheduleBot/Reminder/" + user_id + "reminder.csv")
+    return rows
+
+
+def turn_reminder_to_string(user_id):
+    """
+    Function: turn_types_to_string
+    Description: Reads the event types file and turns all of them into a formatted string
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output:
+        output - Formatted string of rows in event types file
+    """
+    output = ""
+    space = [12, 5]
+    rows = read_type_file(user_id)
+    line_number = 0
+    for i in rows:
+        if line_number != 0:
+            output += f"{i[0]:<{space[0]}}{i[1]:<{space[1]}}\n"
+        line_number += 1
+    return output
+
+
+
+
+
+def create_key_directory():
+    """
+    Function: create_event_directory
+    Description: Creates ScheduleBot event directory in users Documents folder if it doesn't exist
+    Input: None
+    Output: Creates Event folder if it doesn't exist
+    """
+    if not os.path.exists(os.path.expanduser("~/Documents/ScheduleBot/Key")):
+        Path(os.path.expanduser("~/Documents/ScheduleBot/Key")).mkdir(parents=True, exist_ok=True)
+
+
+
+def check_key(user_id):
+    """
+    Function: check_key
+    Description: Creates ScheduleBot event key in users Documents folder if it doesn't exist
+    Input: user_id String representing the Discord ID of the user
+    Output: the key for the given user
+    """
+    create_key_directory()
+    if not os.path.exists(os.path.expanduser("~/Documents") + "/ScheduleBot/Key/" + user_id + ".key"):
+        key = write_key(user_id)
+    else:
+        key = load_key(user_id)
+
+    return key
+
+
+def write_key(user_id):
+    """
+    Function: write_key
+    Description: Generate the key for the user
+    Input:
+        user_id - String representing the Discord ID of the user
+    Output: the writen key
+    """
+    #Generates a key and save it into a file
+    key = Fernet.generate_key()
+    with open(os.path.expanduser("~/Documents") + "/ScheduleBot/Key/" + user_id + ".key", "wb") as key_file:
+        key_file.write(key)
+    return key
+
+
+def load_key(user_id):
+    """
+    Function: load_key
+    Description: read the key for the user
+    Input:
+        userid - String representing the Discord ID of the user
+    Output: the loaded key
+    """
+    with open(os.path.expanduser("~/Documents") + "/ScheduleBot/Key/" + user_id + ".key","rb") as filekey:
+        key = filekey.read()
+
+    return key
+
+
+def encrypt_file(key, filepath):
+    """
+    Function: encrypt_file
+    Description: encrypt the given file with the given key
+    Input:
+        key - key to encrypt
+        filepath - filepath to encrypt
+    Output: None
+    """
+    # using the generated key
+    fernet = Fernet(key)
+    # opening the original file to encrypt
+    with open(filepath, 'rb') as file:
+        original = file.read()
+
+    # encrypting the file
+    encrypted = fernet.encrypt(original)
+
+    # opening the file in write mode and
+    # writing the encrypted data
+    with open(filepath, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+
+def decrypt_file(key, filepath):
+    """
+    Function: decrypt_file
+    Description: decrypt the given file with the given key
+    Input:
+        key - key to decrypt
+        filepath - filepath to decrypt
+    Output: None
+    """
+    # using the key
+    fernet = Fernet(key)
+
+    # opening the encrypted file
+    with open(filepath, 'rb') as enc_file:
+        encrypted = enc_file.read()
+
+    # decrypting the file
+    decrypted = fernet.decrypt(encrypted)
+
+    # opening the file in write mode and
+    # writing the decrypted data
+    with open(filepath, 'wb') as dec_file:
+        dec_file.write(decrypted)
